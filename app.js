fc2b70b3dbd69adfad61bd5b3c4d5895e3e2ee30
msalves008 <matheusdesousaalves2018@gmail.com>
2020-08-13 21:10:53 -0300

    criando Rotas de Pedidos

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 
+// produtos
 const rotaProdutos = require('./routes/produtos')
-
 /*o servidor recebe as requisições do tipo /produtos e procura o tipo 
 da requisição (post, get, delete,...) dentro de rotas produtos */
 app.use('/produtos', rotaProdutos) 
 
-
+//pedidos
+const rotaPedidos = require('./routes/pedidos')
+app.use('/pedidos', rotaPedidos)
 
 
 module.exports = app
---
+++ routes/pedidos.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+
+// retorna todos os pedidos 
+router.get('/', (req, res, next)=>{
+    res.status(200).send({
+        mensagem: "dentro do GET da rota de pedidos"
+    })
+})
+
+//insere um pedido
+router.post('/', (req, res, next)=>{
+    res.status(200).send({
+        mensagem: "usando POST dentro da rota de pedidos"
+    })
+})
+
+// retorna um pedido especifico
+router.get('/:id_pedidos',(req, res,next)=>{
+    const id = req.params.id_pedidos
+
+    if(id ==='especial'){
+        res.status(200).send({
+            mensagem: 'Você descubriu um ID Especial'
+        })
+    }else{
+        res.status(200).send({
+            mensagem: "buscando produt com Id",
+            id: id
+        })
+    }    
+})
+
+//Atualiza um pedido
+router.patch('/:id_pedidos', (req, res, next)=>{
+    res.status(201).send({
+        mensagem: "usando PATCH dentro da rota de pedidos"
+    })
+})
+
+//Deleta um pedido
+router.delete('/:id_pedidos', (req, res, next)=>{
+    const id = req.params.id_pedidos
+    res.status(201).send({
+        mensagem: "usando DELETE dentro da rota de pedidos",
+        id : id 
+    })
+})
+
+
+
+
+
+
+
+
+module.exports = router
