07bd94f035d324d5924d8f770d9d86cb41e05396
msalves008 <matheusdesousaalves2018@gmail.com>
2020-08-22 02:25:18 -0300

    Finalizando cadastro de usuario e validando usuarios já cadastrado

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,30 +6,41 @@ const bcrypt = require('bcrypt')
 
 router.post('/cadastro',(req,res, next)=>{
     mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send({ error: error }) }
-        bcrypt.hash(req.body.senha, 10 ,(errBcrypt, hash)=>{// gera um hash da senha do usuario de 10 caracteres 
-            if (errBcrypt){
-                return res.status(500).send({error: errBcrypt})
-            }
-            conn.query(
-                `INSERT INTO usuarios (email, senha) VALUES (?,?)`,
-                [ req.body.email,hash],
-                (error, results)=>{
-                    conn.release()
-                    if (error) { 
-                        return res.status(500).send({ error: error }) 
+        // verifica se já existe o email no banco de dados
+        conn.query('SELECT * FROM  usuarios WHERE email =?',[req.body.email],(error,results)=>{
+            if (error) { return res.status(500).send({ error: error }) }
+            if(results.length> 0){
+                res.status(409).send({
+                    mensagem: 'Usuário já cadastrado'
+                })
+            } else{
+                if (error) { return res.status(500).send({ error: error }) }
+                bcrypt.hash(req.body.senha, 10 ,(errBcrypt, hash)=>{// gera um hash da senha do usuario de 10 caracteres 
+                    if (errBcrypt){
+                        return res.status(500).send({error: errBcrypt})
                     }
-                   const response = {
-                        mensagem: 'Usuário criado com sucesso',
-                        usuarioCriado: {
-                            id_usuario: results.insertId,
-                            email: req.body.email
+                    conn.query(
+                        `INSERT INTO usuarios (email, senha) VALUES (?,?)`,
+                        [ req.body.email,hash],
+                        (error, results)=>{
+                            conn.release()
+                            if (error) { 
+                                return res.status(500).send({ error: error }) 
+                            }
+                           const response = {
+                                mensagem: 'Usuário criado com sucesso',
+                                usuarioCriado: {
+                                    id_usuario: results.insertId,
+                                    email: req.body.email
+                                }
+                            }
+                            return res.status(201).send(response)
                         }
-                    }
-                    return res.status(201).send(response)
-                }
-                )
-        }) 
+                        )
+                })
+            }
+        })
+       
     })
 })
 
